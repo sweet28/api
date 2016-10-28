fef2734dce98c40da8089c667722617d69a1fa03
sweet28 <shiyifengyoucan@163.com>
2016-10-28 20:06:24 +0800

    token延期

--- src/main/java/com/arttraining/commons/util/TokenUtil.java
+++ src/main/java/com/arttraining/commons/util/TokenUtil.java
@@ -3,6 +3,7 @@ package com.arttraining.commons.util;
 import java.util.Date;
 
 public class TokenUtil {
+	//todo:生成token
 	public static String generateToken(String account, String pwd){
 		String token = "1d8edeb159e97a8213359031b93985b6";
 		String nowTime = System.currentTimeMillis() + "";
@@ -12,7 +13,15 @@ public class TokenUtil {
 		return token;
 	}
 	
+	//todo:验证token是否有效
 	public static boolean checkToken(String token){
+		boolean flag = false;
+		
+		return flag;
+	}
+	
+	//todo:将token有效期延期，token默认有效期为7天，当天操作过token将有效期延期一天
+	public static boolean delayTokenDeadline(String token){
 		boolean flag = false;
 		
 		return flag;
