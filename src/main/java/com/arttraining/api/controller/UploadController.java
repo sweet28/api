3266384b5cfac8c74477702bc244b0ebda2b3741
sweet28 <shiyifengyoucan@163.com>
2016-11-02 19:52:17 +0800

    上传接口获取七牛云token

--- src/main/java/com/arttraining/api/controller/UploadController.java
+++ src/main/java/com/arttraining/api/controller/UploadController.java
@@ -1,5 +1,8 @@
 package com.arttraining.api.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,35 +18,46 @@ import com.qiniu.util.Auth;
 @RequestMapping("/upload")
 public class UploadController {
 	// 依据用户uid查询爱好者用户信息
-	@RequestMapping(value = "/get_token/qiniu/{access_token}", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	public @ResponseBody Object getQiniuToken(
-			@PathVariable("access_token") String token) {
+	@RequestMapping(value = "/get_token/qiniu", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	public @ResponseBody Object getQiniuToken(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("银子来了");
+		String accessToken = "";
+		String uid = "";
 		String QNToken = "";
 		String errorCode = "";
 		String errorMessage = "";
-		//todo:判断token是否有效
-		boolean tokenFlag = TokenUtil.checkToken(token);
-		if(tokenFlag){
-			Auth auth = Auth.create(ConfigUtil.QINIU_AK, ConfigUtil.QINIU_SK);
-			QNToken = auth.uploadToken(ConfigUtil.QINIU_BUCKET, null, ConfigUtil.QINIU_EXPIRES, ConfigUtil.QINIU_POLICY);
-			if(QNToken.equals("") || QNToken == null){
-				errorCode = "";
-				errorMessage = "";
-				QNToken = "";
-			}else{
-				errorCode = "0";
-				errorMessage = "ok";
-			}
+		
+		accessToken = request.getParameter("access_token");
+		uid = request.getParameter("uid");
+		if(accessToken.equals("") || accessToken == null || uid.equals("") || uid == null){
+			errorCode = "20042";
+			errorMessage = "获取上传口令失败";
 		}else{
-			errorCode = "20039";
-			errorMessage = "登录失效，请重新登录";
+			//todo:判断token是否有效
+			boolean tokenFlag = TokenUtil.checkToken(accessToken);
+			if(tokenFlag){
+				Auth auth = Auth.create(ConfigUtil.QINIU_AK, ConfigUtil.QINIU_SK);
+				QNToken = auth.uploadToken(ConfigUtil.QINIU_BUCKET, null, ConfigUtil.QINIU_EXPIRES, ConfigUtil.QINIU_POLICY);
+				if(QNToken.equals("") || QNToken == null){
+					errorCode = "";
+					errorMessage = "";
+					QNToken = "";
+				}else{
+					errorCode = "0";
+					errorMessage = "ok";
+				}
+				System.out.println("银子来了2");
+			}else{
+				errorCode = "20039";
+				errorMessage = "登录失效，请重新登录";
+			}
 		}
 
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put(ConfigUtil.PARAMETER_ERROR_CODE, errorCode);
 		jsonObject.put(ConfigUtil.PARAMETER_ERROR_MSG, errorMessage);
 		jsonObject.put(ConfigUtil.PARAMETER_QINIU_TOKEN, QNToken);
-
+		System.out.println("银子来了3："+jsonObject.toJSONString());
 		return jsonObject;
 
 	}
--- src/main/java/com/arttraining/commons/util/ConfigUtil.java
+++ src/main/java/com/arttraining/commons/util/ConfigUtil.java
@@ -8,7 +8,7 @@ public class ConfigUtil {
 	public static String QINIU_SK = "ZxF06uDuQ_ZQVKsNSQykLN1YwWYrC1lFTunBUFf6";
 	public static String QINIU_BUCKET = "yipei-2016";//空间名
 	public static String QINIU_KEY = null;
-	public static long QINIU_EXPIRES = 3600;//有效时长，单位秒
+	public static long QINIU_EXPIRES = 86400;//有效时长，单位秒
 	public static boolean STRICT = true; 
 	public static StringMap QINIU_POLICY = null;
 	public static String QINIU_COM_URL = "oflkt0ank.bkt.clouddn.com";
