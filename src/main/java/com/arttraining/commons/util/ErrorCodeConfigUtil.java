bf2e08017fe6a9a46f74ff7eb85648963797b190
sweet28 <shiyifengyoucan@163.com>
2016-11-03 13:59:36 +0800

    错误代码说明描述工具类

---
+++ src/main/java/com/arttraining/commons/util/ErrorCodeConfigUtil.java
@@ -0,0 +1,61 @@
+package com.arttraining.commons.util;
+
+public class ErrorCodeConfigUtil {
+	public static String ERROR_MSG_ZH_10001 = "系统错误";
+	public static String ERROR_MSG_ZH_10002 = "服务暂停";
+	public static String ERROR_MSG_ZH_10003 = "远程服务错误";
+	public static String ERROR_MSG_ZH_10004 = "IP限制不能请求该资源";
+	public static String ERROR_MSG_ZH_10005 = "该资源需要app拥有授权";
+	public static String ERROR_MSG_ZH_10006 = "缺少source参数";
+	public static String ERROR_MSG_ZH_10007 = "不支持的MediaType (%s)";
+	public static String ERROR_MSG_ZH_10008 = "参数错误，请参考API文档";
+	public static String ERROR_MSG_ZH_10009 = "任务过多，系统繁忙";
+	public static String ERROR_MSG_ZH_10010 = "任务超时";
+	public static String ERROR_MSG_ZH_10011 = "RPC错误";
+	public static String ERROR_MSG_ZH_10012 = "非法请求";
+	public static String ERROR_MSG_ZH_10013 = "不合法的用户";
+	public static String ERROR_MSG_ZH_10014 = "应用的接口访问权限受限";
+	public static String ERROR_MSG_ZH_10015 = "缺失必选参数 (%s)，请参考API文档";
+	public static String ERROR_MSG_ZH_10016 = "参数值非法";
+	public static String ERROR_MSG_ZH_10017 = "请求长度超过限制";
+	public static String ERROR_MSG_ZH_10018 = "接口不存在";
+	public static String ERROR_MSG_ZH_10019 = "请求的HTTP METHOD不支持，请检查是否选择了正确的POST/POST方式";
+	public static String ERROR_MSG_ZH_10020 = "IP请求频次超过上限";
+	public static String ERROR_MSG_ZH_10021 = "用户请求频次超过上限";
+	public static String ERROR_MSG_ZH_10022 = "用户请求特殊接口 (%s) 频次超过上限";
+	
+	public static String ERROR_MSG_ZH_20001 = "IDs参数为空";
+	public static String ERROR_MSG_ZH_20002 = "Uid参数为空";
+	public static String ERROR_MSG_ZH_20003 = "用户不存在";
+	public static String ERROR_MSG_ZH_20004 = "不支持的图片类型，仅仅支持JPG、GIF、PNG";
+	public static String ERROR_MSG_ZH_20005 = "图片太大";
+	public static String ERROR_MSG_ZH_20006 = "请确保使用multpart上传图片";
+	public static String ERROR_MSG_ZH_20007 = "内容为空";
+	public static String ERROR_MSG_ZH_20008 = "IDs参数太长了";
+	public static String ERROR_MSG_ZH_20009 = "输入文字太长，请确认不超过140个字符";
+	public static String ERROR_MSG_ZH_20010 = "输入文字太长，请确认不超过300个字符";
+	public static String ERROR_MSG_ZH_20011 = "安全检查参数有误，请再调用一次";
+	public static String ERROR_MSG_ZH_20012 = "账号、IP或应用非法，暂时无法完成此操作";
+	public static String ERROR_MSG_ZH_20013 = "发布内容过于频繁";
+	public static String ERROR_MSG_ZH_20014 = "提交相似的信息";
+	public static String ERROR_MSG_ZH_20015 = "包含非法网址";
+	public static String ERROR_MSG_ZH_20016 = "提交相同的信息";
+	public static String ERROR_MSG_ZH_20017 = "包含广告信息";
+	public static String ERROR_MSG_ZH_20018 = "包含非法内容";
+	public static String ERROR_MSG_ZH_20019 = "此IP地址上的行为异常";
+	public static String ERROR_MSG_ZH_20020 = "需要验证码";
+	public static String ERROR_MSG_ZH_20021 = "发布成功，目前服务器可能会有延迟，请耐心等待1-2分钟";
+	
+	public static String ERROR_MSG_ZH_20022 = "账户不存在";
+	public static String ERROR_MSG_ZH_20023 = "密码错误";
+	public static String ERROR_MSG_ZH_20024 = "该手机号已经注册";
+	public static String ERROR_MSG_ZH_20025 = "验证码发送失败";
+	public static String ERROR_MSG_ZH_20026 = "验证码输入不正确";
+	public static String ERROR_MSG_ZH_20027 = "小组名称重复";
+	public static String ERROR_MSG_ZH_20028 = "登录失效，请重新登录";
+	public static String ERROR_MSG_ZH_20029 = "已经收藏了";
+	public static String ERROR_MSG_ZH_20030 = "已经点赞了";
+	public static String ERROR_MSG_ZH_20031 = "获取上传口令失败";
+	public static String ERROR_MSG_ZH_20032 = "数据参数为空，请检查网络是否畅通";
+
+}
