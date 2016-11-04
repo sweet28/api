4e986f484e18f2e332b1a3dfb452ff957218d1cd
sweet28 <shiyifengyoucan@163.com>
2016-11-04 11:32:53 +0800

    专业表增添

---
+++ src/main/java/com/arttraining/api/dao/MajorMapper.java
@@ -0,0 +1,17 @@
+package com.arttraining.api.dao;
+
+import com.arttraining.api.pojo.Major;
+
+public interface MajorMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Major record);
+
+    int insertSelective(Major record);
+
+    Major selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Major record);
+
+    int updateByPrimaryKey(Major record);
+}
