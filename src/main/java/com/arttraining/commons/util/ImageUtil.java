3bc68a9026e50345a8666c0ebc650e9ea3d3a9a4
sweet28 <shiyifengyoucan@163.com>
2016-11-17 14:34:50 +0800

    图片处理类

---
+++ src/main/java/com/arttraining/commons/util/ImageUtil.java
@@ -0,0 +1,56 @@
+package com.arttraining.commons.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.arttraining.api.bean.HomePageAttBean;
+
+public class ImageUtil {
+	// 封装一个方法用于解析json数据 然后将其拆解
+	public static String parseAttPath(String store_path) {
+		String path = "";
+		// 首先判断是否是Json
+		JSONArray jsonArray = JSONArray.parseArray(store_path);
+		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
+			JSONObject jsonObject = (JSONObject) iterator.next();
+			path = jsonObject.getString("store_path");
+		}
+
+		return path;
+	}
+
+	// 封装一个方法用于解析json数据 然后将其拆解
+	public static String parseQiNiuPath(String store_path) {
+		String path = "";
+		// 首先判断是否是Json
+		JSONArray jsonArray = JSONArray.parseArray(store_path);
+		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
+			JSONObject jsonObject = (JSONObject) iterator.next();
+			path = ConfigUtil.QINIU_BUCKET_COM_URL + "/"
+					+ jsonObject.getString("store_path");
+		}
+
+		return path;
+	}
+	
+	public static String parsePicPath(String store_path){
+		String result = "";
+		
+		if(!"".equals(store_path) && null != store_path){
+			result = ConfigUtil.QINIU_BUCKET_COM_URL+"/" + store_path;
+		}else{
+			result = store_path;
+		}
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
