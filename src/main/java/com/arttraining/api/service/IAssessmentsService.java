30ca8a434264c1b14196d8c13f43e5ca3c9cb6de
sweet28 <shiyifengyoucan@163.com>
2016-11-24 17:05:12 +0800

    ceping artforyou

---
+++ src/main/java/com/arttraining/api/service/IAssessmentsService.java
@@ -0,0 +1,9 @@
+package com.arttraining.api.service;
+
+import com.arttraining.api.pojo.Assessments;
+
+public interface IAssessmentsService {
+	int insert(Assessments assessments);
+
+	int update(Assessments assessments);
+}
