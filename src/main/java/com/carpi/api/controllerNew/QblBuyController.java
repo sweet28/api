dfe61b6a13b7b456c6a768926450c9e5a55f7261
29188 <29188@DESKTOP-GK3ADLS>
2018-06-05 22:52:40 +0800

    购买券

--- src/main/java/com/carpi/api/controllerNew/QblBuyController.java
+++ src/main/java/com/carpi/api/controllerNew/QblBuyController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.arttraining.commons.util.JsonResult;
-import com.arttraining.commons.util.TimeUtil;
 import com.carpi.api.pojo.QuanBaoLiRecord;
 import com.carpi.api.service.QblBuyService;
 
@@ -20,15 +19,51 @@ public class QblBuyController {
 
 	@Autowired
 	private QblBuyService qblBuyService;
-	
-//	// 购买券宝理商品券(一型矿机数量)
-//	@RequestMapping(value = "/yx", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-//	@ResponseBody
-//	public JsonResult buyqbl(HttpServletRequest request, HttpServletResponse response) {
-//		
-//		//name   position 额度   day周期
-//		QuanBaoLiRecord quanBaoLiRecord = new QuanBaoLiRecord();
-//		quanBaoLiRecord.setCreateDate(TimeUtil.getTimeStamp());
-//		return quanBaoLiRecordService.xiangQing(Integer.valueOf(id));
-//	}
+
+	// 购买券宝理商品券(一型矿机数量)
+	@RequestMapping(value = "/yx", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public JsonResult buyqbl(HttpServletRequest request, HttpServletResponse response) {
+		// 粉丝id
+		String fensUserId = request.getParameter("uid");
+		// 券id
+		String qid = request.getParameter("id");
+		
+		QuanBaoLiRecord quanBaoLiRecord = new QuanBaoLiRecord();
+		quanBaoLiRecord.setFensUserId(Integer.valueOf(fensUserId));
+		quanBaoLiRecord.setQuanId(Integer.valueOf(qid));
+		return qblBuyService.buyqbl(quanBaoLiRecord);
+	}
+
+	// 购买券宝理商品券(一型矿机数量)
+	@RequestMapping(value = "/ex", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public JsonResult buyqbl2(HttpServletRequest request, HttpServletResponse response) {
+		// 粉丝id
+		String fensUserId = request.getParameter("uid");
+		// 券id
+		String qid = request.getParameter("id");
+
+		// name position 额度 day周期
+		QuanBaoLiRecord quanBaoLiRecord = new QuanBaoLiRecord();
+		quanBaoLiRecord.setFensUserId(Integer.valueOf(fensUserId));
+		quanBaoLiRecord.setId(Integer.valueOf(qid));
+		return qblBuyService.buyqbl2(quanBaoLiRecord);
+	}
+
+	// 购买券宝理商品券(一型矿机数量)
+	@RequestMapping(value = "/sx", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public JsonResult buyqbl3(HttpServletRequest request, HttpServletResponse response) {
+		// 粉丝id
+		String fensUserId = request.getParameter("uid");
+		// 券id
+		String qid = request.getParameter("id");
+
+		// name position 额度 day周期
+		QuanBaoLiRecord quanBaoLiRecord = new QuanBaoLiRecord();
+		quanBaoLiRecord.setFensUserId(Integer.valueOf(fensUserId));
+		quanBaoLiRecord.setId(Integer.valueOf(qid));
+		return qblBuyService.buyqbl3(quanBaoLiRecord);
+	}
 }
--- src/main/java/com/carpi/api/service/impl/QblBuyServiceImpl.java
+++ src/main/java/com/carpi/api/service/impl/QblBuyServiceImpl.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.arttraining.commons.util.JsonResult;
+import com.arttraining.commons.util.TimeUtil;
 import com.carpi.api.dao.FensMinerMapper;
+import com.carpi.api.dao.FensUserMapper;
+import com.carpi.api.dao.QuanBaoLiMapper;
 import com.carpi.api.dao.QuanBaoLiRecordMapper;
-import com.carpi.api.pojo.FensMiner;
+import com.carpi.api.pojo.FensUser;
+import com.carpi.api.pojo.QuanBaoLi;
 import com.carpi.api.pojo.QuanBaoLiRecord;
 import com.carpi.api.service.QblBuyService;
 
@@ -20,7 +25,12 @@ public class QblBuyServiceImpl implements QblBuyService {
 
 	@Autowired
 	private QuanBaoLiRecordMapper quanBaoLiRecordMapper;
+	
+	@Autowired
+	private FensUserMapper fensUserDao;
 
+	@Autowired
+	private QuanBaoLiMapper quanBaoLiDao;
 	// 购买券宝理商品券(一型矿机)
 	@Override
 	public JsonResult buyqbl(QuanBaoLiRecord quanBaoLiRecord) {
@@ -35,13 +45,7 @@ public class QblBuyServiceImpl implements QblBuyService {
 		if (!check()) {
 			return JsonResult.build(500, "您今天已经购买过券保理 ，请明天再来");
 		}
-
-		// 插入数据库
-		int result = insert(quanBaoLiRecord);
-		if (result != 1) {
-			return JsonResult.build(500, "提交失败");
-		}
-		return JsonResult.ok();
+		return insert(quanBaoLiRecord);
 	}
 
 	// 购买券宝理商品券(二型矿机数量)
@@ -55,13 +59,7 @@ public class QblBuyServiceImpl implements QblBuyService {
 		if (!check()) {
 			return JsonResult.build(500, "您今天已经购买过券保理 ，请明天再来");
 		}
-
-		// 插入数据库
-		int result = insert(quanBaoLiRecord);
-		if (result != 1) {
-			return JsonResult.build(500, "提交失败");
-		}
-		return JsonResult.ok();
+		return insert(quanBaoLiRecord);
 	}
 
 	// 购买券宝理商品券(三型矿机数量)
@@ -75,13 +73,7 @@ public class QblBuyServiceImpl implements QblBuyService {
 		if (!check()) {
 			return JsonResult.build(500, "您今天已经购买过券保理 ，请明天再来");
 		}
-
-		// 插入数据库
-		int result = insert(quanBaoLiRecord);
-		if (result != 1) {
-			return JsonResult.build(500, "提交失败");
-		}
-		return JsonResult.ok();
+		return insert(quanBaoLiRecord);
 	}
 
 	// 没人每天只能购买一张券
@@ -94,11 +86,45 @@ public class QblBuyServiceImpl implements QblBuyService {
 	}
 
 	// 购买插入数据库
-	public int insert(QuanBaoLiRecord quanBaoLiRecord) {
-		return quanBaoLiRecordMapper.insertSelective(quanBaoLiRecord);
+	public JsonResult insert(QuanBaoLiRecord quanBaoLiRecord) {
+
+		if (StringUtils.isEmpty(quanBaoLiRecord.getFensUserId())) {
+			return JsonResult.build(500, "系统错误");
+		}
+		if (StringUtils.isEmpty(quanBaoLiRecord.getQuanId())) {
+			return JsonResult.build(500, "不存在此订单");
+		}
+		//查询粉丝信息
+		FensUser fensUser = fensUserDao.selectByPrimaryKey(quanBaoLiRecord.getFensUserId());
+		if (StringUtils.isEmpty(fensUser)) {
+			return JsonResult.build(500, "不存在用户会员信息");
+		}
+		//查询券信息
+		QuanBaoLi quanBaoLi = quanBaoLiDao.selectByPrimaryKey(quanBaoLiRecord.getQuanId());
+		if (StringUtils.isEmpty(quanBaoLi)) {
+			return JsonResult.build(500, "不存在此券信息");
+		}
+		quanBaoLiRecord.setCreateDate(TimeUtil.getTimeStamp());
+		quanBaoLiRecord.setName(fensUser.getName());
+		//额度
+		quanBaoLiRecord.setPosition(quanBaoLi.getPosition());
+		//天数
+		quanBaoLiRecord.setDay(quanBaoLi.getDay());
+		//收益比例
+		quanBaoLiRecord.setEarnProportion(quanBaoLi.getEarnProportion());
+		//截止日期
+		quanBaoLiRecord.setDeadline(quanBaoLi.getDeadline());
+        //出局额度
+		quanBaoLiRecord.setOutPrice(Double.valueOf(quanBaoLi.getBak1()));
+		//购买提交时间
+		quanBaoLiRecord.setBuySubDate(TimeUtil.getTimeStamp());
+		//订单状态  1：购买待匹配
+		quanBaoLiRecord.setOrderType(1);
+		int result = quanBaoLiRecordMapper.insertSelective(quanBaoLiRecord);
+		if (result != 1) {
+			return JsonResult.build(500, "提交失败");
+		}
+		return JsonResult.ok(result);
 	}
-	
-	
-	
 
 }
